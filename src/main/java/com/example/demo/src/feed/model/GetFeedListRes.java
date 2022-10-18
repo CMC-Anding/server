4d1d9d16c8ac4d1d969b2b3de191e608f3b5b070
benjamin <chosj1526@gmail.com>
2022-10-18 12:38:33 +0900

    feat: 피드 게시글 목록 조회 API

--- src/main/java/com/example/demo/src/feed/model/GetFeedListRes.java
+++ src/main/java/com/example/demo/src/feed/model/GetFeedListRes.java
@@ -1,4 +1,4 @@
-package com.example.demo.src.post.model;
+package com.example.demo.src.feed.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -7,13 +7,14 @@ import lombok.Setter;
 @Getter
 @Setter
 @AllArgsConstructor
-public class GetPostDetailRes {
-    private String contents;
+
+public class GetFeedListRes {
+    private int postId;
     private String dailyTitle;
     private String qnaBackgroundColor;
-    private String filterId;
+    private String filterId; //전체필터에서 필터별 이미지 분류 출력을 위함
     private String qnaQuestionId; //추후 질문별 이미지 다르게 할 경우 대비
-    private String qnaQuestion;
+    private String qnaQuestion; 
     private String dailyImage;
     private String qnaQuestionMadeFromUser;
 }
